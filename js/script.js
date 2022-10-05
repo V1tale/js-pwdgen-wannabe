d4dc785e2dcbc32cee1f9e1c753687cf2550f158
V1tale <elativ.c@gmail.com>
2022-10-05 14:54:02 +0200

    raccolta dati

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Input
+const insertName = prompt ("inserisci il tuo nome");
+const insertLastName = prompt ("Inserisci il tuo cognome");
+const insertFavouriteColor = prompt ("il tuo colore preferito");
+console.log(insertName, typeof insertName)
+// PROCESSING
+
+// OUTPUT
