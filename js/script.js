ea20815a99335b49875087f02bba64bc4450c7e2
V1tale <elativ.c@gmail.com>
2022-10-05 15:13:07 +0200

    js completato

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 const insertName = prompt ("inserisci il tuo nome");
 const insertLastName = prompt ("Inserisci il tuo cognome");
 const insertFavouriteColor = prompt ("il tuo colore preferito");
-console.log(insertName, typeof insertName)
+console.log(insertName, typeof insertName);
 // PROCESSING
-
+const result = `La tua password è: ${insertName}${insertLastName}${insertFavouriteColor}21`
 // OUTPUT
+document.getElementById ("password").innerHTML = result;
