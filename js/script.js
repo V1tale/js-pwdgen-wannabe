0088db61da8a83ef1c5e691751e0f4771674823d
V1tale <elativ.c@gmail.com>
2022-10-05 15:42:10 +0200

    stilizzazione completata

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ const insertLastName = prompt ("Inserisci il tuo cognome");
 const insertFavouriteColor = prompt ("il tuo colore preferito");
 console.log(insertName, typeof insertName);
 // PROCESSING
-const result = `La tua password è: ${insertName}${insertLastName}${insertFavouriteColor}21`
+const result = ` ${insertName}${insertLastName}${insertFavouriteColor}21`
 // OUTPUT
 document.getElementById ("password").innerHTML = result;
